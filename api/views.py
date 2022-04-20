64da0f0642d131756014523370ca1ebf4e80573f
André Moreno <deco616@hotmail.com>
2022-04-20 14:16:39 -0300

    Retorno GET consultas

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -20,6 +21,9 @@ class ScheduleView(APIView):
 
 class AppointmentView(APIView):
     def get(self, request):
-        appoitments = Appointment.objects.all()
-        serializer = AppointmentSerializer(appoitments, many=True)
+        appointments = Appointment.objects.all()
+        result = [d for d in appointments if d.dia >= date.today()]
+        result = sorted(result, key=lambda result: result.horario)
+        result = sorted(result, key=lambda result: result.dia)
+        serializer = AppointmentSerializer(result, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
